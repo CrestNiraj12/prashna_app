14af9de55fd397587da4a4c9837a0fe615d09517
CrestNiraj12 <crestniraj@gmail.com>
2022-08-29 00:14:46 +0545

    minor

--- lib/utilities/api.dart
+++ lib/utilities/api.dart
@@ -2,9 +2,9 @@ import 'package:dio/dio.dart';
 
 Dio dio() {
   Dio dio = Dio();
-  // dio.options.baseUrl = "https://studypill.org/api";
+  dio.options.baseUrl = "https://studypill.org/api";
 
-  dio.options.baseUrl = "http://192.168.10.169:8000/api";
+  // dio.options.baseUrl = "http://192.168.10.169:8000/api";
 
   dio.options.headers['accept'] = 'application/json';
   dio.options.connectTimeout = 60 * 1000;
