f13f98b707a32e55288d2ee2993ff039272b11c1
CrestNiraj12 <crestniraj@gmail.com>
2022-08-28 23:15:54 +0545

    added search in home

--- lib/screens/discover_screen/discover_screen.dart
+++ lib/screens/discover_screen/discover_screen.dart
@@ -19,6 +19,8 @@ class DiscoverScreen extends StatefulWidget {
 class _DiscoverScreenState extends State<DiscoverScreen> {
   late List _courses;
   bool _loading = true;
+  final _searchController = TextEditingController();
+  List<Course> _allCourses = [];
 
   @override
   void initState() {
@@ -33,6 +35,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
 
     setState(() {
       _courses = courses;
+      _allCourses = courses;
       _loading = false;
     });
   }
@@ -41,6 +44,71 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
   Widget build(BuildContext context) {
     final currTheme = Provider.of<Auth>(context, listen: true);
     final user = currTheme.user;
+    TextStyle searchStyle = const TextStyle(
+        fontSize: 16, color: PRIMARY_DARK, fontFamily: 'Montserrat');
+
+    TextStyle style = const TextStyle(
+        fontSize: 24.0,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Montserrat');
+
+    // Input field for search
+    final TextField searchField = TextField(
+      controller: _searchController,
+      textInputAction: TextInputAction.search,
+      obscureText: false,
+      style: searchStyle.copyWith(
+          color: Provider.of<Auth>(context, listen: false).darkTheme
+              ? Colors.white
+              : PRIMARY_DARK),
+      decoration: InputDecoration(
+          hintText: "Search",
+          suffixIcon: _searchController.text.isEmpty
+              ? null
+              : IconButton(
+                  onPressed: () {
+                    _searchController.clear();
+                    setState(() {
+                      _courses = _allCourses;
+                    });
+                  },
+                  icon: const Icon(Icons.clear)),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          fillColor: Provider.of<Auth>(context, listen: true).darkTheme
+              ? PRIMARY_DARK
+              : Colors.white,
+          filled: true,
+          prefixIcon: const Icon(
+            Icons.search,
+            color: PRIMARY_BLUE,
+            size: 18,
+          ),
+          focusColor: PRIMARY_BLUE,
+          border: const OutlineInputBorder(borderSide: BorderSide.none)),
+      onSubmitted: (String text) async {
+        setState(() {
+          _loading = true;
+        });
+
+        if (text.isEmpty) {
+          setState(() {
+            _courses = _allCourses;
+            _loading = false;
+          });
+        } else {
+          Response response =
+              await dio().get("/search/courses/q=${text.trim().toLowerCase()}");
+          setState(() {
+            _courses = response.data
+                .map<Course>((data) => Course.fromJson(data))
+                .toList();
+            _loading = false;
+          });
+        }
+      },
+    );
 
     Widget _createCard(course, {required double width, double height = 100.0}) {
       return Card(
@@ -85,12 +153,6 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
       );
     }
 
-    TextStyle style = const TextStyle(
-        fontSize: 24.0,
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-        fontFamily: 'Montserrat');
-
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -103,7 +165,9 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
               onTap: () => Navigator.push(
                   context,
                   PageTransition(
-                      child: user == null ? LoginScreen() : ProfileScreen(),
+                      child: user == null
+                          ? const LoginScreen()
+                          : const ProfileScreen(),
                       type: PageTransitionType.fade)),
               child: Padding(
                 padding: const EdgeInsets.only(right: 10.0),
@@ -128,52 +192,80 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
             child: SingleChildScrollView(
           child: SizedBox(
             width: MediaQuery.of(context).size.width,
-            child: _loading
-                ? SizedBox(
-                    height: MediaQuery.of(context).size.height - 100,
-                    child: const Center(
-                        child: CircularProgressIndicator(
-                      color: PRIMARY_BLUE,
-                    )),
-                  )
-                : Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 20),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
                     child: Column(
-                      children: [
-                        ...List<int>.generate((_courses.length / 2).round(),
-                            (int index) => index * 2).map(
-                          (i) => Container(
-                            margin: const EdgeInsets.only(bottom: 5),
-                            child: (i + 2) <= _courses.length
-                                ? Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                        ..._courses.sublist(i, i + 2).map(
-                                            (item) => _createCard(item,
-                                                width: MediaQuery.of(context)
-                                                            .size
-                                                            .width /
-                                                        2 -
-                                                    18))
-                                      ])
-                                : Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                        ..._courses.sublist(i, i + 1).map(
-                                            (item) => _createCard(item,
-                                                width: MediaQuery.of(context)
-                                                            .size
-                                                            .width /
-                                                        2 -
-                                                    18))
-                                      ]),
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Find courses",
+                              style: searchStyle.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                  color: PRIMARY_BLUE)),
+                          const SizedBox(
+                            height: 10,
                           ),
-                        )
-                      ],
-                    ),
+                          searchField
+                        ]),
                   ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _loading
+                      ? SizedBox(
+                          height: MediaQuery.of(context).size.height - 300,
+                          child: const Center(
+                              child: CircularProgressIndicator(
+                            color: PRIMARY_BLUE,
+                          )),
+                        )
+                      : Column(
+                          children: [
+                            ...List<int>.generate((_courses.length / 2).round(),
+                                (int index) => index * 2).map(
+                              (i) => Container(
+                                margin: const EdgeInsets.only(bottom: 5),
+                                child: (i + 2) <= _courses.length
+                                    ? Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                            ..._courses.sublist(i, i + 2).map(
+                                                (item) => _createCard(item,
+                                                    width:
+                                                        MediaQuery.of(context)
+                                                                    .size
+                                                                    .width /
+                                                                2 -
+                                                            18))
+                                          ])
+                                    : Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                            ..._courses.sublist(i, i + 1).map(
+                                                (item) => _createCard(item,
+                                                    width:
+                                                        MediaQuery.of(context)
+                                                                    .size
+                                                                    .width /
+                                                                2 -
+                                                            18))
+                                          ]),
+                              ),
+                            )
+                          ],
+                        )
+                ],
+              ),
+            ),
           ),
         )));
   }
